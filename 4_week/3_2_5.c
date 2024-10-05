1f584ba24c3f08cc698d4e6cae87d087c4d980f1
Alexaner <alexander.ovodkov.ao@yandex.ru>
2024-10-05 18:53:48 +0300

    3.2.5

---
+++ 4_week/3_2_5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node* ptr;
+};
+
+typedef struct node Node;
+
+int main() {
+    Node* node1 = (Node*)malloc(sizeof(Node));
+    Node* node2 = (Node*)malloc(sizeof(Node));
+
+    node1->value = 10;
+    node1->ptr = node2;
+
+    node2->value = 20;
+    node2->ptr = node1;
+
+    printf("Node 1: value = %d, ptr -> value = %d\n", node1->value, node1->ptr->value);
+    printf("Node 2: value = %d, ptr -> value = %d\n", node2->value, node2->ptr->value);
+
+    free(node1);
+    free(node2);
+
+    return 0;
+}
