f9b53c81b91791950312132ca5ee124b829544d5
Alexaner <alexander.ovodkov.ao@yandex.ru>
2024-10-05 10:47:48 +0300

    3.1.4

---
+++ 4_week/3_1_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void mult2(int *p, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        *(p + i) += *(p + i);
+    }
+}
+
+void mult2_b(int *p, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        p[i] *= 2;
+    }
+}
+
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    size_t n = sizeof(arr) / sizeof(arr[0]);
+    mult2(arr, n);
+    // mult2_b(arr, n);
+    for (size_t i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
