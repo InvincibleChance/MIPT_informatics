a1f8ea17b08b970886862b0ebc9175c9d8c6d455
Alexaner <alexander.ovodkov.ao@yandex.ru>
2024-10-11 21:26:55 +0300

    1.1.5

---
+++ 5_week/1.1.5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct book {
+    char title[50];
+    int pages;
+    float price;
+};
+
+typedef struct book Book;
+
+
+void print_book(Book *b) {
+    printf("Title: %s\n", b->title);
+    printf("Pages: %d\n", b->pages);
+    printf("Price: %.2f\n", b->price);
+}
+
+int main() {
+    Book **p = (Book **) malloc(sizeof(Book *));
+    *p = (Book *) malloc(sizeof(Book));
+
+    strcpy((*p)->title, "Don Quixote");
+    (*p)->pages = 1000;
+    (*p)->price = 750.0;
+
+    print_book(*p);
+    free(p);
+}
